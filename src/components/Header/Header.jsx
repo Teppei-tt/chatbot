c49771cd26ad89eba08409a49ceb6ac99110687f
Teppei-tt <teppei.takata03@gmail.com>
2021-07-11 20:13:04 +0900

    Headerにボタン要素を追加及びスタイリング

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,9 @@ import React from "react"
 export const Header = () => {
   return (
     <header className="header">
-      <div>
-        <div className="header_left">MyApp</div>
+      <div className="header_left">MyApp</div>
+      <div className="header_light">
+        <a href="" className="btn btn--orange">ログイン</a>
       </div>
     </header>
   )
