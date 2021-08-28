3650d4bbf8594ec6014cee2e48f64f5fb1944b8a
Teppei-tt <teppei.takata03@gmail.com>
2021-08-28 21:29:53 +0900

    ✨ Formの部分をデザインだけ作成

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -5,10 +5,15 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import TextField from '@material-ui/core/TextField';
+
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    maxWidth: 600,
+    marginTop: "10%",
+    margin: "auto",
   },
   bullet: {
     display: "inline-block",
@@ -25,29 +30,14 @@ const useStyles = makeStyles({
 
 export const Login = () => {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.root}>
       <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Word of the Day
+        <Typography gutterBottom variant="h5" component="h2" >
+          Lizard
         </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
